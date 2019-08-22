d91e2861135f5ddd34dcded721908340371a9383
WinZip <wydo@protonmail.com>
2019-08-22 13:21:55 +0300

    Updates

---
+++ precache-manifest.f76ace1290284868b2e23e19fe312edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8239261fc01b63a61091ebd58a682c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c7465d96e92589e2902",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d86164b43bde39872300",
+    "url": "./static/css/main.780277f7.chunk.css"
+  },
+  {
+    "revision": "9c7465d96e92589e2902",
+    "url": "./static/js/2.bd30bc96.chunk.js"
+  },
+  {
+    "revision": "d86164b43bde39872300",
+    "url": "./static/js/main.95046c9b.chunk.js"
+  },
+  {
+    "revision": "3ba06d001e847cd56812",
+    "url": "./static/js/runtime~main.82eb172f.js"
+  }
+]);
